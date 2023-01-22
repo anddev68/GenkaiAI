eaeadc4d8d09bcf5d466d96702046559e3e18f0b
mr68shi <chk392013@gmail.com>
2023-01-22 16:04:04 +0900

    pages向けにstaticファイル追加

---
+++ out/_next/static/R4tjHZTTZJivVk8i1zqKD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/281-95c01f0775116b4f.js","static/css/b006b331397fd480.css","static/chunks/pages/index-e0eb68f6e9b5ced3.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
